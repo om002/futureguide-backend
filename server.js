dab4ac8a62dc51397251ff126d5e39acb6374ff7
TheFutureGuide <omkm1157@gmail.com>
2025-11-04 23:41:14 +0530

    Fixed MySQL pool and duplicate app.listen

--- server.js
+++ server.js
@@ -42,13 +42,6 @@ app.get("/test-db", (req, res) => {
   
 
 
-db.connect((err) => {
-  if (err) {
-    console.error(" Error connecting to MySQL:", err);
-  } else {
-    console.log("Connected to MySQL");
-  }
-});
 
 
 
@@ -119,9 +112,7 @@ app.post("/login", (req, res) => {
 
 
 //  Start Server
-app.listen(3000, () => {
-  console.log(" Server running on port 3000");
-});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
